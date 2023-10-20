ad97b2458d6ea2a93be26c1847848a45c0fe87f6
MasterCoookie <jas.kocurek2@wp.pl>
2023-10-20 13:46:22 +0200

    somewhat functional communication, requires socket unification tho

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ class Initiator:
         self.port = port
         self.uwb_address = uwb_address
 
+    def bind_sckt(self):
         self.sckt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sckt.bind(('0.0.0.0', 12))
+        self.sckt.settimeout(1)
 
     def send(self, message):
         self.sckt.sendto(message.encode(), (self.ip, self.port))
@@ -20,19 +22,28 @@ class Initiator:
             print(data.decode())
         except socket.timeout:
             print("Timeout Occured.")
-        self.sckt.close()
 
 
 
 if __name__ == "__main__":
     tag_a = Initiator("192.168.0.112", 7, "DD")
-    # tag_b = Initiator("192.168.0.113", 7, "EE")
+    tag_b = Initiator("192.168.0.113", 7, "EE")
     anchors = ("AA", "BB")
-    for i in range(1):
+    for i in range(10):
         time.sleep(0.1)
         anchor = random.choice(anchors)
+
+        tag_a.bind_sckt()
         tag_a.send(anchor)
         tag_a.receive()
-        # time.sleep(0.1)
-        # anchor = random.choice(anchors)
-        # tag_b.send(anchor)
+        tag_a.receive()
+        tag_a.sckt.close()
+
+        time.sleep(0.1)
+
+        anchor = random.choice(anchors)
+        tag_b.bind_sckt()
+        tag_b.send(anchor)
+        tag_b.receive()
+        tag_b.receive()
+        tag_b.sckt.close()
