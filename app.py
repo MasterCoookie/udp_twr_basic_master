cea15239869299bd804632df988f614c7d2c4851
MasterCoookie <jas.kocurek2@wp.pl>
2023-10-22 13:30:38 +0200

    cleanup begins

--- app.py
+++ app.py
@@ -3,30 +3,42 @@ import time
 import random
 
 class Initiator:
-    def __init__(self, ip, port, uwb_address, sckt):
+    def __init__(self, ip, port, uwb_address):
         self.ip = ip
         self.port = port
         self.uwb_address = uwb_address
-        self.sckt = sckt
 
-class UDPSocket:
-    def __init__(self, out_port):
+# class UDPSocket:
+#     def __init__(self, out_port):
+#         self.bound_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#         self.bound_socket.bind(('0.0.0.0', out_port))
+#         self.bound_socket.settimeout(1)
+#         # open file
+#         self.file = open(f'data_{out_port}.txt', "w+")
+
+#     def send(self, message, ip, port):
+#         self.bound_socket.sendto(message.encode(), (ip, port))
+
+#     def receive(self, uwb_address):
+#         try:
+#             data = self.bound_socket.recv(1024)
+#             # print(data.decode())
+#             self.file.write(f'{uwb_address}: {data.decode()}')
+#         except socket.timeout:
+#             self.file.write(f'{uwb_address}: Timeout\n')
+
+class UDPSharedSocket:
+    def __init__(self, out_port, dvices_no):
         self.bound_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.bound_socket.bind(('0.0.0.0', out_port))
         self.bound_socket.settimeout(1)
-        # open file
-        self.file = open(f'data_{out_port}.txt', "w+")
+        self.results_buffer = [[]] * dvices_no
 
-    def send(self, message, ip, port):
+    def send(self, message, ip, port, verbose=False):
+        if verbose:
+            print(f'Sending {message} to {ip}:{port}')
         self.bound_socket.sendto(message.encode(), (ip, port))
 
-    def receive(self, uwb_address):
-        try:
-            data = self.bound_socket.recv(1024)
-            # print(data.decode())
-            self.file.write(f'{uwb_address}: {data.decode()}')
-        except socket.timeout:
-            self.file.write(f'{uwb_address}: Timeout\n')
 
 
 
@@ -35,19 +47,19 @@ if __name__ == "__main__":
     freq = 50
     delay = 1/(freq/2)
 
-    tag_a = Initiator("192.168.0.13", 13, "DD", UDPSocket(13))
-    tag_b = Initiator("192.168.0.112", 12, "EE", UDPSocket(12))
+    # tag_a = Initiator("192.168.0.112", 7, "DD", UDPSocket(12))
+    tag_b = Initiator("192.168.0.113", 7, "EE", UDPSocket(1547))
     start = time.time()
     for i in range(100):
         # time.sleep(1/freq)
         anchor = random.choice(anchors)
 
-        print(f'Sending from {tag_a.uwb_address} to {anchor}')
-        tag_a.sckt.send(anchor, tag_a.ip, tag_a.port)
-        tag_a.sckt.receive(tag_a.uwb_address)
-        tag_a.sckt.receive(tag_a.uwb_address)
+        # print(f'Sending from {tag_a.uwb_address} to {anchor}')
+        # tag_a.sckt.send(anchor, tag_a.ip, tag_a.port)
+        # tag_a.sckt.receive(tag_a.uwb_address)
+        # tag_a.sckt.receive(tag_a.uwb_address)
 
-        # time.sleep(delay)
+        time.sleep(delay)
         anchor = random.choice(anchors)
 
         print(f'Sending from {tag_b.uwb_address} to {anchor}')
