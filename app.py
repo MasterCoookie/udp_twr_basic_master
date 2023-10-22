64e9957357127f73c2b81033df87ef97a94d5e8e
MasterCoookie <jas.kocurek2@wp.pl>
2023-10-22 13:45:44 +0200

    adds file dumping

--- app.py
+++ app.py
@@ -20,28 +20,33 @@ class UDPSharedSocket:
             print(f'Sending {message} to {ip}:{port}')
         self.bound_socket.sendto(message.encode(), (ip, port))
 
-    def receive(self, result_pre, device_index, verbose=False):
+    def receive(self, result_prefix, device_index, verbose=False):
         try:
             data = self.bound_socket.recv(1024)
             if verbose:
-                print(f'{result_pre} received {data.decode()}')
-            self.results_buffer[device_index].append(f'{result_pre} received {data.decode()}')
+                print(f'{result_prefix} received {data.decode()}')
+            self.results_buffer[device_index].append(f'{result_prefix} received {data.decode()}')
             
         except socket.timeout:
             if verbose:
-                print(f'{result_pre} timeout')
-            self.results_buffer[device_index].append(f'{result_pre} timeout')
+                print(f'{result_prefix} timeout')
+            self.results_buffer[device_index].append(f'{result_prefix} timeout')
+
+    def dump_results(self, file_prefix):
+        for i, results in enumerate(self.results_buffer):
+            with open(f'{file_prefix}_{i}.txt', 'w') as f:
+                for result in results:
+                    f.write(f'{result}\n')
 
 
 
 if __name__ == "__main__":
     anchors = ("AA", "BB")
-
-    # tag_a = Initiator("192.168.0.112", 7, "DD", UDPSocket(12))
-    # tag_b = Initiator("192.168.0.113", 7, "EE", UDPSocket(1547))
     tags = (Initiator("192.168.0.112", 7, "DD"), Initiator("192.168.0.113", 12, "EE"))
     sckt = UDPSharedSocket(12, len(tags))
+
     start = time.time()
+
     for exchange_index in range(100):
         for i, tag in enumerate(tags):
             anchor = random.choice(anchors)
@@ -49,7 +54,8 @@ if __name__ == "__main__":
             sckt.receive(f'#{exchange_index}. {tag.uwb_address} to {anchor}', i, True)
             sckt.receive(f'#{exchange_index}. {tag.uwb_address} to {anchor}', i, True)
     
-    sckt.bound_socket.close()
-    
     end = time.time() - start
+    sckt.bound_socket.close()
+    sckt.dump_results('results')
+    
     print(f'Time elapsed: {end}')
